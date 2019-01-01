c41a03b7eeeae6db2aba6c618df038a76db5c2ef
Roland Edwards <rol.edwards@live.com>
2019-01-01 14:08:15 +0000

    added CSV file

--- db.sql
+++ db.sql
@@ -4,24 +4,28 @@ create database arabic;
 use arabic;
 
 create table vocab (
-	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id serial PRIMARY KEY,
 	arabic varchar(128) NOT NULL,
-	english int(11) NOT NULL,
-	)
+	english varchar(128) NOT NULL
+	);
+
 	
 create table users (
-	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	id serial PRIMARY KEY,
 	username varchar(128) NOT NULL,
 	password varchar(128) NOT NULL,
-)
+);
 
 create table users_vocab (
-	id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	user_id,
-	word_id,
-	score int(11) NOT NULL;
-)
+	id serial PRIMARY KEY,
+	user_id integer not null,
+	word_id integer not null,
+	score integer not null
+);
 
+/*
+???
 foreign key (user_id) references users(id),
 foreign key (word_id) references vocab(id)
+*/
 
